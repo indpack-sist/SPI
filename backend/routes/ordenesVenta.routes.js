a36b7be405b989b34f19d70dc229529a9aaaff8c
jaimeyxv <jaimeyxv@gmail.com>
2025-12-31 15:59:50 -0500

    fix name

--- backend/routes/ordenesVenta.routes.js
+++ backend/routes/ordenesVenta.routes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { verificarToken } from '../middlewares/auth.js';
+import { verificarToken } from '../middleware/auth.js';
 import {
   getAllOrdenesVenta,
   getOrdenVentaById,

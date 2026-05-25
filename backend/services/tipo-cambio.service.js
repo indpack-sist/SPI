a661e41f797125f0c2bf285e575a6053af2e8bd3
indpack-sist <jaimeyxv@gmail.com>
2026-05-25 09:25:16 -0500

    tipo cambio service

--- backend/services/tipo-cambio.service.js
+++ backend/services/tipo-cambio.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const DECOLECTA_API_URL = 'https://api.decolecta.com/v1';
-const DECOLECTA_API_KEY = process.env.DECOLECTA_API_KEY;
+const DECOLECTA_API_KEY = process.env.API_DECOLECTA_TOKEN || process.env.DECOLECTA_API_KEY;
 const API_TIMEOUT = 10000;
 
 let tipoCambioCache = {
